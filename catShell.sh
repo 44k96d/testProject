b41a08002c0bdcf55054fecfad4477847d87a1cc
44k96d <44k96d@gmail.com>
2015-12-18 13:33:28 +0900

    Update catShell.sh

--- catShell.sh
+++ catShell.sh
@@ -6,3 +6,4 @@ find . -name "*.java" -or -name "*.cpp" -or -name "*.h" -or -name "*.c" | while 
     cat license.txt hoge.txt > $hoge;
     svn propset svn:mime-type "text/plain; charset=utf-8" $hoge
 done
+//d.hatena.ne.jp/billiejoe/20091001 を参考にフォルダにセットしてもよい
